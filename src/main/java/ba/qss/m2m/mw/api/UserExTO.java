8b30cb957c5b7d53a69aa196d1765a0698a1325d
aselimovic <adnan@qss.ba>
2021-09-10 14:58:18 +0200

    constructor UserExTO column annotation

--- src/main/java/ba/qss/m2m/mw/api/UserExTO.java
+++ src/main/java/ba/qss/m2m/mw/api/UserExTO.java
@@ -1,8 +1,11 @@
 package ba.qss.m2m.mw.api;
 
+import javax.persistence.Column;
+
 import ba.qss.framework.dataaccess.UserTO;
 
 public class UserExTO extends UserTO {
+	@Column(name="profile_id", insertable=false, updatable=false)	
 	private int profileId;
 	
 	// getters
